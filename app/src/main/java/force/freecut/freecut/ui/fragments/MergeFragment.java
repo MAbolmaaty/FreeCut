8e26145ef1b5eba9593992517a1fbc41193205ff
Muhammad7 <muhammad.mu.july@gmail.com>
2021-11-01 00:01:43 +0200

    selecting multiple videos for merge

--- app/src/main/java/force/freecut/freecut/ui/fragments/MergeFragment.java
+++ app/src/main/java/force/freecut/freecut/ui/fragments/MergeFragment.java
@@ -2,12 +2,21 @@ package force.freecut.freecut.ui.fragments;
 
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.ClipData;
+import android.content.ContentUris;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.storage.StorageManager;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -42,6 +51,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.lang.reflect.Array;
+import java.lang.reflect.Method;
 import java.net.URL;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
@@ -263,52 +274,79 @@ public class MergeFragment extends Fragment {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == OPEN_MEDIA_PICKER) {
             // Make sure the request was successful
-            if (resultCode == RESULT_OK && data != null) {
-                mProgressDialog = new ProgressDialog(getActivity());
-                mProgressDialog.setTitle(getString(R.string.preparing));
+            if (resultCode == RESULT_OK) {
+                ClipData clipData = data.getClipData();
+                if (clipData != null) {
+                    for (int i = 0 ; i < clipData.getItemCount() ; i++){
+                        ClipData.Item videoItem = clipData.getItemAt(i);
+                        Uri videoURI = videoItem.getUri();
+                        String filePath = getPath(getActivity(), videoURI);
+                        mVideoItem = new VideoItem();
+                        mVideoItem.setItem_name(new File(filePath).getName());
+                        mVideoItem.setImageLink(filePath);
+                        mListVideos.add(mVideoItem);
+                        All = "";
+                        mVideosAdapter = new VideosAdapter(getActivity(), mListVideos,
+                                VIDEOS_MERGE, null, new VideoItemDeleteHandler() {
+                            @Override
+                            public void onClick(int position) {
+                                mListVideos.remove(position);
+                                mVideosAdapter.notifyDataSetChanged();
+                            }
+                        });
+                        mRecyclerView.setAdapter(mVideosAdapter);
+                        mRecyclerView.setVisibility(View.VISIBLE);
 
-                mProgressDialog.setCancelable(false);
-                mProgressDialog.setMessage(getString(R.string.waiting));
-                //mProgressDialog.show();
-                final ArrayList<String> selectionResult = data.getStringArrayListExtra("result");
-                ind = 0;
-                for (final String item : selectionResult) {
-
-                    /*final String[] command = {"-i", item};
-                    FFmpeg.executeAsync(command, new ExecuteCallback() {
-                        @Override
-                        public void apply(long executionId, int returnCode) {
-                            if (returnCode == RETURN_CODE_SUCCESS) {
-                                ind = ind + 1;*/
-                                /*if (All.contains("Audio:") && All.contains("Video:") && !All.contains("Invalid data found when processing input")) {
-                                    */mVideoItem = new VideoItem();
-                                    mVideoItem.setItem_name(new File(item).getName());
-                                    mVideoItem.setImageLink(item);
-                                    mListVideos.add(mVideoItem);
-                                    All = "";
-                                /*} else {
-                                    All = "";
-                                }*/
-
-                                    mVideosAdapter = new VideosAdapter(getActivity(), mListVideos,
-                                            VIDEOS_MERGE, null, new VideoItemDeleteHandler() {
-                                        @Override
-                                        public void onClick(int position) {
-                                            mListVideos.remove(position);
-                                            mVideosAdapter.notifyDataSetChanged();
-                                        }
-                                    });
-                                    mRecyclerView.setAdapter(mVideosAdapter);
-                                    mProgressDialog.dismiss();
-                                    mImport.setBackgroundTintList(ContextCompat.
-                                            getColorStateList(getActivity(), R.color.grey));
-                                    mMerge.setBackgroundTintList(ContextCompat.
-                                            getColorStateList(getActivity(), R.color.orange));
-
-                           /* }
-                        }
-                    });*/
+                    }
+                } else {
+                    Toast.makeText(getActivity(), getString(R.string.select_multiple_videos),
+                            Toast.LENGTH_SHORT).show();
                 }
+//                mProgressDialog = new ProgressDialog(getActivity());
+//                mProgressDialog.setTitle(getString(R.string.preparing));
+//
+//                mProgressDialog.setCancelable(false);
+//                mProgressDialog.setMessage(getString(R.string.waiting));
+//                //mProgressDialog.show();
+//                final ArrayList<String> selectionResult = data.getStringArrayListExtra("result");
+//                ind = 0;
+//                for (final String item : selectionResult) {
+//
+//                    /*final String[] command = {"-i", item};
+//                    FFmpeg.executeAsync(command, new ExecuteCallback() {
+//                        @Override
+//                        public void apply(long executionId, int returnCode) {
+//                            if (returnCode == RETURN_CODE_SUCCESS) {
+//                                ind = ind + 1;*/
+//                                /*if (All.contains("Audio:") && All.contains("Video:") && !All.contains("Invalid data found when processing input")) {
+//                                    */mVideoItem = new VideoItem();
+//                                    mVideoItem.setItem_name(new File(item).getName());
+//                                    mVideoItem.setImageLink(item);
+//                                    mListVideos.add(mVideoItem);
+//                                    All = "";
+//                                /*} else {
+//                                    All = "";
+//                                }*/
+//
+//                                    mVideosAdapter = new VideosAdapter(getActivity(), mListVideos,
+//                                            VIDEOS_MERGE, null, new VideoItemDeleteHandler() {
+//                                        @Override
+//                                        public void onClick(int position) {
+//                                            mListVideos.remove(position);
+//                                            mVideosAdapter.notifyDataSetChanged();
+//                                        }
+//                                    });
+//                                    mRecyclerView.setAdapter(mVideosAdapter);
+//                                    mProgressDialog.dismiss();
+//                                    mImport.setBackgroundTintList(ContextCompat.
+//                                            getColorStateList(getActivity(), R.color.grey));
+//                                    mMerge.setBackgroundTintList(ContextCompat.
+//                                            getColorStateList(getActivity(), R.color.orange));
+//
+//                           /* }
+//                        }
+//                    });*/
+//                }
             }
         }
     }
@@ -371,5 +409,188 @@ public class MergeFragment extends Fragment {
         } finally {
             is.close();
         }
+    }
+
+    private String getPath(final Context context, final Uri uri) {
+
+        final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+
+        // DocumentProvider
+        if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
+
+            // ExternalStorageProvider
+
+            if (isExternalStorageDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                if ("primary".equalsIgnoreCase(type)) {
+                    return Environment.getExternalStorageDirectory() + "/" + split[1];
+                } else {
+                    // Below logic is how External Storage provider build URI for documents
+                    // Based on http://stackoverflow.com/questions/28605278/android-5-sd-card-label and https://gist.github.com/prasad321/9852037
+                    StorageManager mStorageManager = (StorageManager) context.getSystemService(Context.STORAGE_SERVICE);
+
+                    try {
+                        Class<?> storageVolumeClazz = Class.forName("android.os.storage.StorageVolume");
+                        Method getVolumeList = mStorageManager.getClass().getMethod("getVolumeList");
+                        Method getUuid = storageVolumeClazz.getMethod("getUuid");
+                        Method getState = storageVolumeClazz.getMethod("getState");
+                        Method getPath = storageVolumeClazz.getMethod("getPath");
+                        Method isPrimary = storageVolumeClazz.getMethod("isPrimary");
+                        Method isEmulated = storageVolumeClazz.getMethod("isEmulated");
+
+                        Object result = getVolumeList.invoke(mStorageManager);
+
+                        final int length = Array.getLength(result);
+                        for (int i = 0; i < length; i++) {
+                            Object storageVolumeElement = Array.get(result, i);
+                            //String uuid = (String) getUuid.invoke(storageVolumeElement);
+
+                            final boolean mounted = Environment.MEDIA_MOUNTED.equals(getState.invoke(storageVolumeElement))
+                                    || Environment.MEDIA_MOUNTED_READ_ONLY.equals(getState.invoke(storageVolumeElement));
+
+                            //if the media is not mounted, we need not get the volume details
+                            if (!mounted) continue;
+
+                            //Primary storage is already handled.
+                            if ((Boolean) isPrimary.invoke(storageVolumeElement) && (Boolean) isEmulated.invoke(storageVolumeElement))
+                                continue;
+
+                            String uuid = (String) getUuid.invoke(storageVolumeElement);
+
+                            if (uuid != null && uuid.equals(type)) {
+                                String res = getPath.invoke(storageVolumeElement) + "/" + split[1];
+                                return res;
+                            }
+                        }
+                    } catch (Exception ex) {
+                    }
+                }
+            }
+
+
+            // DownloadsProvider
+            else if (isDownloadsDocument(uri)) {
+
+                final String id = DocumentsContract.getDocumentId(uri);
+                if (!TextUtils.isEmpty(id)) {
+                    if (id.startsWith("raw:")) {
+                        return id.replaceFirst("raw:", "");
+                    }
+                }
+                String[] contentUriPrefixesToTry = new String[]{
+                        "content://downloads/public_downloads",
+                        "content://downloads/my_downloads",
+                        "content://downloads/all_downloads"
+                };
+
+                for (String contentUriPrefix : contentUriPrefixesToTry) {
+                    Uri contentUri = ContentUris.withAppendedId(Uri.parse(contentUriPrefix), Long.valueOf(id));
+                    try {
+                        String path = getDataColumn(context, contentUri, null, null);
+                        if (path != null) {
+                            return path;
+                        }
+                    } catch (Exception e) {
+                    }
+                }
+
+                return null;
+            }
+            // MediaProvider
+            else if (isMediaDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                Uri contentUri = null;
+                if ("image".equals(type)) {
+                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                } else if ("video".equals(type)) {
+                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                } else if ("audio".equals(type)) {
+                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                }
+
+                final String selection = "_id=?";
+                final String[] selectionArgs = new String[]{
+                        split[1]
+                };
+
+                return getDataColumn(context, contentUri, selection, selectionArgs);
+            }
+        }
+        // MediaStore (and general)
+        else if ("content".equalsIgnoreCase(uri.getScheme())) {
+
+//            // Return the remote address
+            if (isGooglePhotosUri(uri))
+                return uri.getLastPathSegment();
+
+            return getDataColumn(context, uri, null, null);
+        }
+        // File
+        else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
+        }
+
+        return null;
+    }
+
+    public static boolean isGooglePhotosUri(Uri uri) {
+        return "com.google.android.apps.photos.content".equals(uri.getAuthority());
+    }
+
+    /**
+     * Get the value of the data column for this Uri.
+     */
+    private String getDataColumn(Context context, Uri uri, String selection,
+                                 String[] selectionArgs) {
+
+        Cursor cursor = null;
+        final String column = "_data";
+        final String[] projection = {
+                column
+        };
+
+        try {
+            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs,
+                    null);
+            if (cursor != null && cursor.moveToFirst()) {
+                final int column_index = cursor.getColumnIndexOrThrow(column);
+                return cursor.getString(column_index);
+            }
+        } finally {
+            if (cursor != null)
+                cursor.close();
+        }
+        return null;
+    }
+
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is ExternalStorageProvider.
+     */
+    private boolean isExternalStorageDocument(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is DownloadsProvider.
+     */
+    private boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is MediaProvider.
+     */
+    private boolean isMediaDocument(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri.getAuthority());
     }
 }
